272afa377e3478a3dad0fbb7cf7383b39ca83a00
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-08-04 10:39:13 -0400

    Slight change
    Fixed s.t. there is no requirement to include all terms in final output formulas

--- R/formula_list.R
+++ R/formula_list.R
@@ -10,33 +10,32 @@ formula_list <- function(y, x) {
   n <- length(x)
   all_formulas <- list()
 
-  # Additive terms
+  # Individual terms
   for (i in 1:n) {
+    all_formulas <- c(all_formulas, paste0(y, "~", x[i]))
+  }
+
+  # Additive and interaction terms
+  for (i in 2:n) {
     combinations <- combn(x, i)
     for (j in 1:ncol(combinations)) {
       combination <- combinations[,j]
-      all_formulas <- c(all_formulas, paste(y, "~", paste0(combination, collapse = "+")))
-    }
-  }
 
-  # Interaction terms
-  for (i in 2:n) {
-    combinations_interaction <- combn(x, i)
-    for (j in 1:ncol(combinations_interaction)) {
-      combination_interaction <- combinations_interaction[,j]
-      interaction_formula <- paste(combination_interaction, collapse = "*")
+      # Additive terms
+      all_formulas <- c(all_formulas, paste0(y, "~", paste(combination, collapse="+")))
 
-      # Additive terms with each interaction term
-      for (k in 1:(n-1)) {
-        combinations_additive <- combn(x, k)
+      # Interaction term
+      interaction_formula <- paste(combination, collapse="*")
+      all_formulas <- c(all_formulas, paste0(y, "~", interaction_formula))
+
+      # Additive terms with interaction term
+      for (k in 1:(i-1)) {
+        combinations_additive <- combn(combination, k)
         for (l in 1:ncol(combinations_additive)) {
           combination_additive <- combinations_additive[,l]
-          all_formulas <- c(all_formulas, paste0(y, "~", paste(combination_additive, collapse = "+"), "+", interaction_formula))
+          all_formulas <- c(all_formulas, paste0(y, "~", paste(combination_additive, collapse="+"), "+", interaction_formula))
         }
       }
-
-      # Only interaction term
-      all_formulas <- c(all_formulas, paste0(y, "~", interaction_formula))
     }
   }
 
