61e86e275050c96731deb1b2c480d9ebe2039529
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-12-05 11:12:48 -0500

    DSA and change_agegrp_chunk

--- R/respond_inputManipulations.R
+++ R/respond_inputManipulations.R
@@ -1,3 +1,7 @@
+#' %in% negation
+#' @export
+`%ni%` <- Negate(`%in%`)
+
 #' A Function to Change RESPOND inputs from base-case by some percent change
 #' @param data dataframe: dataframe from RESPOND base input
 #' @param filter string: treatment block to target the change
@@ -13,4 +17,55 @@ DSA <- function(data, filter, cycle, pct_change){
     ][, paste0("to_corresponding_post_trt_cycle", c) := 1 - get(paste0("to_", filter, "_cycle", c))]
   }
   return(DT)
+}
+
+#' A function to change default age groups from RESPOND shell tables
+#' @param data dataframe or datatable: A RESPOND table
+#' @param size.out Integer: age-group size out
+#' @param transformation String: either "mean" or "sum" defining the transformation to new age groups, eg. counts would be summed, transition probabilities would be averaged
+#' @param cols List: columns to transform
+#' @param grouping List: columns to group on, the default is any columns not listed in \code{cols}
+#'
+#' @import data.table
+#' @export
+change_agegrp_chunk <- function(data, size.out, transformation, cols, grouping){
+  DT <- as.data.table(data)
+  if(!cols %in% names(DT)) stop("Column names in 'cols' do not match names in 'data'")
+  if(missing(grouping)) grouping <- names(DT)[c(names(DT) %ni% c(cols, "agegrp"))]
+
+  DT <- DT[, `:=` (age_min = as.integer(sub("_(.*)", "", agegrp)),
+                   age_max = as.integer(sub("^(.*?)_", "", agegrp)),
+                   agegrp = NULL)]
+
+  chunk.diffs <- unique(DT[["age_min"]] - DT[["age_max"]])
+  if(length(chunk.diffs) > 1){
+    stop("Chunk differences non-uniform")
+  } else size.in <- chunk.diffs*-1 + 1
+
+  # A more efficient method would be to Map(seq, age_min, age_max),
+  # although unlisting seems to be a problem.... will work on this in the future
+
+  expanded <- merge(DT, expand.grid(block = unique(DT[["block"]]),
+                                    sex = unique(DT[["sex"]]),
+                                    oud = unique(DT[["oud"]]),
+                                    age = as.integer(c(min(DT[["age_min"]]):max(DT[["age_max"]])))),
+                    by = grouping,
+                    allow.cartesian = TRUE)[age >= age_min & age <= age_max,
+                    ][, `:=` (age_min = NULL,
+                              age_max = NULL,
+                              agegrp = cut(age, seq(9, 100, size.out),
+                                           labels = paste0(seq(10, 98, size.out),
+                                                           "_",
+                                                           seq(11, 99, size.out))),
+                              age = NULL)
+                    ]
+  if(transformation == "sum"){
+    out <- expanded[, (cols) := lapply(cols, function(x) get(x)/size.in),
+    ][, (cols) := lapply(cols, function(x) sum(get(x))), by = c("agegrp", "block", "oud", "sex")]
+    return(unique(out))
+
+  } else if(transformation == "mean"){
+    out <- expanded[, (cols) := lapply(cols, function(x) mean(x))]
+    return(unique(out))
+  }
 }
