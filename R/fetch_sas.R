7650dce84e93597f83eebf0615617062282ab65d
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2024-06-05 20:53:31 -0400

    combine non-needed funcs

--- R/fetch_sas.R
+++ R/fetch_sas.R
@@ -1,13 +1,16 @@
 #' MA DPH PHDW Script Pull
 #' Fetches most recent .SAS files for MA DPH work and saves them to a SAS/ subdirectory
 #'
-#' @importFrom httr GET
+#' @import httr
 #'
 #' @export
 fetch_sas <- function(){
   if(!dir.exists("SAS")) dir.create("SAS")
+  req <- httr::GET("https://api.github.com/repos/SyndemicsLab/PHD/git/trees/main?recursive=1")
+  httr::stop_for_status(req)
 
-  file_list <- list_files_github("SyndemicsLab", "PHD", "main", pattern = ".sas")
+  file_list <- grep(".sas", unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = FALSE),
+                    value = TRUE, fixed = TRUE)
 
   for(i in seq_along(file_list)){
     response <- httr::GET(paste0("https://raw.githubusercontent.com/SyndemicsLab/PHD/main/", file_list[i]),
