aee1e763ea5c784255832a89951ec1e959c529bc
alexandrapurdy <alexandrapurdy23@gmail.com>
2025-07-03 09:25:43 -0400

    Add a another plot to show the change

--- Syndemics/R/compare_moud_counts.R
+++ Syndemics/R/compare_moud_counts.R
@@ -35,3 +35,38 @@ compare_moud_counts(
   "~/Syndemics/Syndemics/Syndemics/MOUD Data/MOUDCount_23JUN2025.csv"
 )
 
+plot_moud_differences <- function(old_path, new_path) {
+  old_df <- read_csv(old_path, show_col_types = FALSE) |>
+    select(treatment, month, year, N_ID) |>
+    rename(old_count = N_ID)
+
+  new_df <- read_csv(new_path, show_col_types = FALSE) |>
+    select(treatment, month, year, N_ID) |>
+    rename(new_count = N_ID)
+
+  diff_df <- left_join(old_df, new_df, by = c("treatment", "month", "year")) |>
+    mutate(
+      difference = new_count - old_count,
+      date = as.Date(paste(year, month, "01", sep = "-"))
+    )
+
+  ggplot(diff_df, aes(x = date, y = difference, fill = difference > 0)) +
+    geom_col(show.legend = FALSE) +
+    facet_wrap(~ treatment, scales = "free_y") +
+    scale_fill_manual(values = c("TRUE" = "darkgreen", "FALSE" = "firebrick")) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    labs(
+      title = "Change in MOUD Counts Over Time (New - Old)",
+      x = "Date",
+      y = "Difference in Count"
+    ) +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1)
+    )
+}
+
+plot_moud_differences(
+  "~/Syndemics/Syndemics/Syndemics/MOUD Data/MOUDCount_03JUN2025.csv",
+  "~/Syndemics/Syndemics/Syndemics/MOUD Data/MOUDCount_23JUN2025.csv"
+)
