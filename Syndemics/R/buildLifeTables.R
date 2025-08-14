f8a53c5d754926be2e65d7b6115cf9d434429b04
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-08-14 17:06:15 -0400

    Sweeps Functions (#23)
    * Update DESCRIPTION
    
    * Delete Syndemics.Rproj
    
    * Update .gitignore
    
    * overhaul of lifetables
    
    * add data.table variable bindings for check clear
    
    * update namespace with imports
    
    * version bump
    
    * quick comment on origin of 100k hardcode

--- Syndemics/R/buildLifeTables.R
+++ Syndemics/R/buildLifeTables.R
@@ -11,6 +11,7 @@
 #' @param bin_size The size ages are grouped by
 #'
 #' @import data.table
+#' @importFrom utils write.csv
 #' @export
 
 build_background_mortality_file <- function(files,
@@ -19,117 +20,89 @@ build_background_mortality_file <- function(files,
                                             sexes = c("female", "male"),
                                             age_groups = c("1_20", "21_40", "41_60", "61_80", "81_100"),
                                             bin_size = 20) {
-    background_mortality <- lapply(files, extract_background_mortality)
-    base_table <- build_base_table()
-    return(fill_base_table(base_table, background_mortality))
+
+  background_mortality <- lapply(files, extract_background_mortality,
+                                 bin_size = bin_size, age_groups = age_groups)
+  result_table <- create_and_fill_table(background_mortality, races, sexes, age_groups)
+  if (!missing(outputfile)) {
+    write.csv(result_table, outputfile, row.names = FALSE)
+  }
+
+  return(result_table)
 }
 
 #' Function used to extract background mortality values based on age from a single yearly CDC NVSS life table
-#'
-#' Input: CDC Life Tables
-#' Output: data.table containing the weekly probabilities
 #'
 #' @param file_path The path to the CDC NVSS Life Table
 #' @param bin_size The size ages are grouped by
 #' @param age_groups A list of age groups
 #'
 #' @import data.table
-#' @import readxl
+#' @importFrom readxl read_excel
 #' @keywords internal
 
 extract_background_mortality <- function(file_path, bin_size = 20, age_groups = c("1_20", "21_40", "41_60", "61_80", "81_100")) {
-    data <- read_excel(file_path, skip = 1)
-    dt <- data.table::as.data.table(data)[(2:101)]
-    setnames(dt, "Probability of dying between ages x and x + 1", "year_prob", skip_absent = TRUE)
-    setnames(dt, "Number dying between ages x and x + 1", "year_deaths", skip_absent = TRUE)
-    year_prob <- NULL
-    dt <- dt[, year_prob := as.numeric(year_prob)]
-    year_deaths <- NULL
-    dt <- dt[, year_deaths := as.numeric(year_deaths)]
-    deaths <- dt[, sum(year_deaths), by = (seq(nrow(dt)) - 1) %/% bin_size][, 2]
-    weekly_rates <- (deaths / 100000) / 52
-    weekly_probs <- 1 - exp(-weekly_rates)
-    colname <- c("agegrp")
-    weekly_probs[, (colname) := age_groups]
-    return(weekly_probs)
+  data <- readxl::read_excel(file_path, skip = 1)
+  dt <- as.data.table(data)[(2:101)]
+
+  # Rename columns to standard names
+  setnames(dt, "Probability of dying between ages x and x + 1", "year_prob", skip_absent = TRUE)
+  setnames(dt, "Number dying between ages x and x + 1", "year_deaths", skip_absent = TRUE)
+  
+  #Data table bindings
+  year_prob <- year_deaths <- V1 <- NULL
+
+  dt[, year_prob := as.numeric(year_prob)
+     ][, year_deaths := as.numeric(year_deaths)]
+
+  bin_groups <- (seq(nrow(dt)) - 1) %/% bin_size
+  deaths_by_group <- dt[, sum(year_deaths), by = bin_groups][, V1]
+
+  # 100k originates from the CDC NVSS data - reported in rates per 100,000 persons
+  weekly_rates <- (deaths_by_group / 100000) / 52
+  weekly_probs <- 1 - exp(-weekly_rates)
+
+  result <- data.table(
+    agegrp = age_groups,
+    weekly_probability = weekly_probs
+  )
+
+  return(result)
 }
 
-#' Build the union data.table of various yearly extracted life probabilities
+#' Create and fill the table with mortality values for all demographic combinations
 #'
-#' Input: List of data.tables containing the weekly probabilities and their stratifications
-#' Output: Joined List of data.tables
-#'
-#' @param lt A list of data tables containing the weekly probabilities
-#'
-#' @import data.table
-#' @keywords internal
-
-build_union <- function(lt) {
-    union <- rbindlist(lt)
-    setnames(union, "V1", "weekly_probability", skip_absent = TRUE)
-    return(union)
-}
-
-#' Build the base table to fill, stratified by age, sex, and race
-#'
-#' Input: Demographics to stratify
-#' Output: Empty Shell table
-#'
+#' @param background_mortality List of extracted background mortality data.tables
 #' @param races A list of races
 #' @param sexes A list of sexes
 #' @param age_groups A list of age groups
 #'
 #' @import data.table
-#' @importFrom tidyr crossing
 #' @keywords internal
 
-build_base_table <- function(races = c("black", "hispanic", "white"),
-                             sexes = c("female", "male"),
-                             age_groups = c("1_20", "21_40", "41_60", "61_80", "81_100")) {
-    crosses <- tidyr::crossing(races, sexes)
-    full_table <- as.data.table(crosses[rep(seq_len(nrow(crosses)), length(age_groups)), ])
-    full_table <- full_table[order(races, sexes)]
-    return(full_table)
+create_and_fill_table <- function(background_mortality,
+                                  races = c("black", "hispanic", "white"),
+                                  sexes = c("female", "male"),
+                                  age_groups = c("1_20", "21_40", "41_60", "61_80", "81_100")) {
+  #Data table bindings
+  agegrp <- NULL
+  
+  combinations <- expand.grid(races = races, sexes = sexes, stringsAsFactors = FALSE)
+  combinations <- as.data.table(combinations)
+  result_table <- combinations[rep(seq_len(nrow(combinations)), each = length(age_groups))]
+  result_table[, agegrp := rep(age_groups, times = nrow(combinations))]
+  n_race_sex_combos <- length(races) * length(sexes)
+
+  mortality_data <- data.table()
+  for (i in seq_along(background_mortality)) {
+    group_index <- ((i - 1) %% n_race_sex_combos) + 1
+    bg_mort <- background_mortality[[i]]
+    demo_info <- combinations[group_index]
+    mortality_group <- cbind(demo_info[rep(1, nrow(bg_mort))], bg_mort)
+    mortality_data <- rbind(mortality_data, mortality_group)
+  }
+
+  setorder(mortality_data, races, sexes, agegrp)
+
+  return(mortality_data)
 }
-
-#' Fill the shell table with values extracted
-#'
-#' Input: Empty shell table and extracted background mortality
-#' Output: a full data.table containing the extracted mortalities
-#'
-#' @param base_table The empty shell table
-#' @param background_mortality The extracted background mortality
-#' @param races A list of races
-#' @param sexes A list of sexes
-#'
-#' @import data.table
-#' @import rlist
-#' @keywords internal
-
-fill_base_table <- function(base_table,
-                            background_mortality,
-                            races = c("black", "hispanic", "white"),
-                            sexes = c("female", "male")) {
-    unions <- list.cbind(lapply(
-        split(
-            background_mortality,
-            ceiling(
-                seq_along(background_mortality) / (length(races) * length(sexes))
-            )
-        ),
-        build_union
-    ))
-    binded <- cbind(base_table, unions)
-    return(binded[, !c("2.agegrp", "3.agegrp", "4.agegrp", "5.agegrp", "6.agegrp", "7.agegrp")])
-}
-
-# Should be the root folder that holds the CDC life tables
-# dirs <- c("~/")
-# files <- list.files(dirs, pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
-
-# End name of the output file
-# outputfile <- "background_mortality.csv"
-
-# Run and Build the Resulting File
-# complete_table <- build_background_mortality_file(files, outputfile)
-# write.csv(complete_table, outputfile)
