8e17a2c531a98a8903ae6efe7675f1201e16ae2d
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-02-23 16:44:03 -0500

    Revert back to `map_dfr()`

--- Syndemics/R/init_cohort_stats.R
+++ Syndemics/R/init_cohort_stats.R
@@ -38,7 +38,7 @@ get_init_cohort_statistics <- function(db_path, table_name) {
     )
 
     init_cohort_table <-
-        purrr::modify(var_names, prop_breakdown, cohort = cohort) |>
+        purrr::map_dfr(var_names, prop_breakdown, cohort = cohort) |>
         select(variable, level, statistic, value) |>
         collect() |>
         bind_rows(age_stats)
