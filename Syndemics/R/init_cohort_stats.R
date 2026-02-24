cc762bd8ede4b3270ca421f3f0d46a8755096b9a
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2026-02-24 09:28:36 -0500

    Attempt to fix outcomes and the final remaining warning

--- Syndemics/R/init_cohort_stats.R
+++ Syndemics/R/init_cohort_stats.R
@@ -60,9 +60,9 @@ get_init_cohort_statistics <- function(db_path, table_name) {
 prop_breakdown <- function(var_name, cohort) {
     prop_breakdown <- cohort |>
         group_by(.data[[var_name]]) |>
-        summarize(n = n(), .groups = "drop") |>
+        summarize(n = n(na.rm = TRUE), .groups = "drop") |>
         mutate(
-            value = as.double(n / sum(n)),
+            value = as.double(n) / sum(n),
             variable = var_name,
             level = as.character(.data[[var_name]]),
             statistic = "proportion",
